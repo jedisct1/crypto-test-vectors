15421a3987de51147134642ed72ba27234d2bb67
Frank Denis <github@pureftpd.org>
2015-12-18 17:05:55 +0100

    + x25519_base generator

---
+++ crypto/scalarmult/x25519/base/generators/libsodium/generator.c
@@ -0,0 +1,40 @@
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sodium.h>
+
+static int
+generate(void)
+{
+    unsigned char scalar[crypto_scalarmult_SCALARBYTES];
+    unsigned char out[crypto_scalarmult_BYTES];
+    char          scalar_hex[crypto_scalarmult_SCALARBYTES * 2U + 1U];
+    char          out_hex[crypto_scalarmult_BYTES * 2U + 1U];
+
+    randombytes_buf(scalar, sizeof scalar);
+    crypto_scalarmult_base(out, scalar);
+    sodium_bin2hex(scalar_hex, sizeof scalar_hex, scalar, sizeof scalar);
+    sodium_bin2hex(out_hex, sizeof out_hex, out, sizeof out);
+    printf("%s\t%s\n", scalar_hex, out_hex);
+
+    return 0;
+}
+
+int
+main(int argc, char *argv[])
+{
+    unsigned long count;
+
+    if (argc < 2) {
+        return 1;
+    }
+    if (sodium_init() != 0) {
+        return 1;
+    }
+    count = strtoul(argv[1], NULL, 10);
+    while (count-- > 0UL) {
+        generate();
+    }
+    return 0;
+}
